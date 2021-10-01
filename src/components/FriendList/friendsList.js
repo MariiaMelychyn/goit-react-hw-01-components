81551467920c83ae1ce1f4b74c674274aa3e02d0
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-10-01 22:45:34 +0300

    remove

--- src/components/FriendList/friendsList.js
+++ src/components/FriendList/friendsList.js
@@ -1,51 +1,40 @@
-import s from './Friend.module.css';
 import PropTypes from 'prop-types';
+import s from './Friend.module.css';
 
-function friendIsOnline(status) {
-  if (status) {
-    return '#2ECC71';
-  } else {
-    return '#E74C3C';
-  }
-}
-export default function FriendsList({ friends }) {
-  return (
-    <div className={s.friends}>
-      <ul className={s.friendList}>
-        {friends.map(friend => (
-          <li className={s.item} key={friend.id}>
-            {' '}
-            <span
-              className={s.status}
-              style={{ color: friendIsOnline(friend.isOnline) }}
-            >
-              {' '}
-              ●
-            </span>
-            <img
-              className={s.avatar}
-              src={friend.avatar}
-              alt={friend.name}
-              width="48"
-            />
-            <p className={s.name}>{friend.name}</p>
+
+
+function FriendList({ friends }) {
+    return (
+  <ul className={s.list}>
+      {friends.map(({ avatar, id, name, isOnline }) => (
+          <li key={id} className={s.item}>
+              <span
+                  className={`${s.status} ${
+                      isOnline ? s.online : s.offline
+                  }`}
+              ></span>
+              <img
+                  className={s.images}
+                  src={avatar}
+                  alt={name}
+                  width="48"
+              />
+              <p className={s.text}>{name}</p>
           </li>
-        ))}
+          ))}
       </ul>
-    </div>
-  );
+    );
 }
 
-FriendsList.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number,
-};
+export default FriendList;
 
-FriendsList.defaultProps = {
-  avatar:
-    'https://st.depositphotos.com/1779253/5140/v/600/depositphotos_51402215-stock-illustration-male-avatar-profile-picture-use.jpg',
-  name: 'User name',
-  isOnline: false,
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.exact({
+            avatar: PropTypes.string.isRequired,
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+        }),
+    ),
 };
